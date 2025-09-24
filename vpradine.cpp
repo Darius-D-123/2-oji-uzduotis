b69a9c0e9f5dbd0448e61a032de305d7578bb91d
Darius-D-123 <KreeperLT123@gmail.com>
2025-09-24 21:30:13 +0300

    Update vpradine.cpp

--- vpradine.cpp
+++ vpradine.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <limits>
 
 using std::cout;
 using std::cin;
@@ -11,50 +12,70 @@ using std::string;
 using std::vector;
 using std::setw;
 using std::left;
-using std::right;
 using std::fixed;
 using std::setprecision;
 
-struct Studentas{
+struct Studentas {
     string vard;
     string pav;
     vector<int> paz;
     int egzas;
-    float rez;
+    double rez_vid;
+    double rez_med;
 };
-Studentas ivesk();
 
-int main(){
+Studentas ivesk();
+double median(vector<int> v);
+
+int main() {
     vector<Studentas> Grupe;
-    for (int j=0; j<3; j++){
-        cout << "Iveskite " << j+1 << " studentas:\n";
+    int kiek;
+    cout << "Kiek studentu norite ivesti? ";
+    cin >> kiek;
+    for (int j = 0; j < kiek; j++) {
+        cout << "\nIveskite " << j + 1 << " studenta:\n";
         Grupe.push_back(ivesk());
     }
     cout << "\nRezultatai:\n";
-    cout << left << setw(15) << "Vardas"
-         << setw(15) << "Pavarde"
-         << setw(10) << "Galutinis" << endl;
-    cout << "------------------------------------------\n";
-
+    cout << left << setw(15) << "Vardas" << setw(15) << "Pavarde" << setw(20) << "Galutinis (Vid.)" << setw(20) << "Galutinis (Med.)" << endl;
+    cout << "--------------------------------------------------------------\n";
     cout << fixed << setprecision(2);
     for (auto temp : Grupe) {
-        cout << setw(15) << temp.vard << setw(15) << temp.pav << setw(10) << temp.rez << endl;
+        cout << setw(15) << temp.vard << setw(15) << temp.pav << setw(20) << temp.rez_vid << setw(20) << temp.rez_med << endl;
     }
 }
 
-Studentas ivesk(){
+Studentas ivesk() {
     Studentas Laik;
-    int sum=0, n, m;
+    int m;
     cout << "Iveskite varda: "; cin >> Laik.vard;
     cout << "Iveskite pavarde: "; cin >> Laik.pav;
-    cout << "Kiek pazymiu turi studentas: "; cin >> n;
-    for (int i=0; i<n; i++) {
-        cout << "Iveskite " << i+1 << " paz. is " << n << " : "; cin >> m;
-        Laik.paz.push_back(m);
-        sum+=m;
+    cout << "Iveskite pazymius (baigti su ne skaiciumi, pvz. ENTER):\n";
+    while (true) {
+        if (!(cin >> m)) {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
         }
-    cout << "Iveskite egzamina: "; cin >> Laik.egzas;
-    Laik.rez=Laik.egzas*0.6 + double(sum)/double(Laik.paz.size())*0.4;
+        Laik.paz.push_back(m);
+    }
+    cout << "Iveskite egzamino rezultata: ";
+    cin >> Laik.egzas;
+    double sum = 0.0;
+    for (int x : Laik.paz) sum += x;
+    double vid = Laik.paz.empty() ? 0 : sum / Laik.paz.size();
+    Laik.rez_vid = 0.6 * Laik.egzas + 0.4 * vid;
+    Laik.rez_med = 0.6 * Laik.egzas + 0.4 * median(Laik.paz);
     return Laik;
 }
 
+double median(vector<int> v) {
+    if (v.empty()) return 0.0;
+    sort(v.begin(), v.end());
+    size_t n = v.size();
+    if (n % 2 == 0) {
+        return (v[n/2 - 1] + v[n/2]) / 2.0;
+    } else {
+        return v[n/2];
+    }
+}
