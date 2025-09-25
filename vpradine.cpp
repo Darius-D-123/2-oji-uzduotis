d2c64d3418f4f3d11545e341ab4e6b7a77541ef0
Darius-D-123 <KreeperLT123@gmail.com>
2025-09-25 16:30:42 +0300

    Update vpradine.cpp 16:30

--- vpradine.cpp
+++ vpradine.cpp
@@ -6,6 +6,7 @@
 #include <limits>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 
 using std::cout;
 using std::cin;
@@ -16,6 +17,9 @@ using std::setw;
 using std::left;
 using std::fixed;
 using std::setprecision;
+using std::sort;
+using std::ifstream;
+using std::ofstream;
 
 struct Studentas {
     string vard;
@@ -28,14 +32,14 @@ struct Studentas {
 
 Studentas ivesk(bool randomMode);
 double median(vector<int> v);
+vector<Studentas> nuskaitykIsFailo(const string &failoVardas);
+void issaugokIFaila(const vector<Studentas> &Grupe, const string &failoVardas);
 
 int main() {
     srand(time(nullptr));
     vector<Studentas> Grupe;
     int kiek;
     char pasirinkimas;
-    cout << "Kiek studentu norite ivesti? ";
-    cin >> kiek;
     do {
     cout << "Pasirinkite veiksma:\n";
     cout << "  t - generuoti atsitiktinai\n";
@@ -45,17 +49,26 @@ int main() {
        } while (pasirinkimas != 't' && pasirinkimas != 'T' &&
              pasirinkimas != 'n' && pasirinkimas != 'N' &&
              pasirinkimas != 'f' && pasirinkimas != 'F');
-    bool randomMode = (pasirinkimas == 't' || pasirinkimas == 'T');
-    for (int j = 0; j < kiek; j++) {
-        cout << "\n--- Studentas " << j + 1 << " ---\n";
-        Grupe.push_back(ivesk(randomMode));
+    if (pasirinkimas == 'f' || pasirinkimas == 'F') {
+        Grupe = nuskaitykIsFailo("studentai10000.txt");
+    } else {
+        int kiek;
+        cout << "Kiek studentu norite ivesti? ";
+        cin >> kiek;
+        bool randomMode = (pasirinkimas == 't' || pasirinkimas == 'T');
+        for (int j = 0; j < kiek; j++) {
+            cout << "\n--- Studentas " << j + 1 << " ---\n";
+            Grupe.push_back(ivesk(randomMode));
+        }
     }
-    cout << "\nRezultatai:\n";
-    cout << left << setw(15) << "Vardas" << setw(15) << "Pavarde" << setw(20) << "Galutinis (Vid.)" << setw(20) << "Galutinis (Med.)" << endl;
-    cout << "--------------------------------------------------------------\n";
-    cout << fixed << setprecision(2);
-    for (auto &s : Grupe) {
-        cout << setw(15) << s.vard << setw(15) << s.pav << setw(20) << s.rez_vid << setw(20) << s.rez_med << endl;
+    if (!Grupe.empty()) {
+        cout << "\nRezultatai:\n";
+        cout << left << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(20) << "Galutinis (Vid.)" << setw(20) << "Galutinis (Med.)" << endl;
+        cout << "---------------------------------------------------------------\n";
+        cout << fixed << setprecision(2);
+        for (auto &s : Grupe) {
+            cout << setw(15) << s.pav << setw(15) << s.vard << setw(20) << s.rez_vid << setw(20) << s.rez_med << endl;
+        }
     }
 }
 
@@ -94,11 +107,57 @@ Studentas ivesk(bool randomMode) {
 
 double median(vector<int> v) {
     if (v.empty()) return 0.0;
-    std::sort(v.begin(), v.end());
+    sort(v.begin(), v.end());
     size_t n = v.size();
     if (n % 2 == 0) {
         return (v[n/2 - 1] + v[n/2]) / 2.0;
     } else {
         return v[n/2];
     }
+}
+
+vector<Studentas> nuskaitykIsFailo(const string &failoVardas) {
+    vector<Studentas> Grupe;
+    ifstream in(failoVardas);
+    if (!in) {
+        cout << "Klaida: nepavyko atidaryti failo " << failoVardas << endl;
+        return Grupe;
+    }
+    string headerLine;
+    getline(in, headerLine);
+    string pav, vard;
+    int nd1, nd2, nd3, nd4, nd5, egz;
+    while (in >> pav >> vard >> nd1 >> nd2 >> nd3 >> nd4 >> nd5 >> egz) {
+        Studentas s;
+        s.pav = pav;
+        s.vard = vard;
+        s.paz = {nd1, nd2, nd3, nd4, nd5};
+        s.egzas = egz;
+        double vid = (nd1 + nd2 + nd3 + nd4 + nd5) / 5.0;
+        s.rez_vid = 0.6 * egz + 0.4 * vid;
+        s.rez_med = 0.6 * egz + 0.4 * median(s.paz);
+        Grupe.push_back(s);
+    }
+    return Grupe;
+}
+
+void issaugokIFaila(const vector<Studentas> &Grupe, const string &failoVardas) {
+    ofstream out(failoVardas);
+    if (!out) {
+        cout << "Klaida: nepavyko sukurti failo " << failoVardas << endl;
+        return;
+    }
+    out << left << setw(15) << "Pavarde"
+        << setw(15) << "Vardas"
+        << setw(20) << "Galutinis (Vid.)"
+        << setw(20) << "Galutinis (Med.)" << endl;
+    out << "---------------------------------------------------------------\n";
+    out << fixed << setprecision(2);
+    for (auto &s : Grupe) {
+        out << setw(15) << s.pav
+            << setw(15) << s.vard
+            << setw(20) << s.rez_vid
+            << setw(20) << s.rez_med << endl;
+    }
+    cout << "Rezultatai issaugoti faile " << failoVardas << endl;
 }
