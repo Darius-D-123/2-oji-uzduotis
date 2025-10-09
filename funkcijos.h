77acc9dfa3be40a0a1c9aa1c527e128ca1c28fa6
Darius-D-123 <KreeperLT123@gmail.com>
2025-10-09 19:23:39 +0300

    Pridėti failai funkcijos.cpp, studentas.h, funkcijos.h po reorganizavimo

---
+++ funkcijos.h
@@ -0,0 +1,31 @@
+#ifndef FUNKCIJOS_H_INCLUDED
+#define FUNKCIJOS_H_INCLUDED
+
+#include <string>
+#include <vector>
+#include "studentas.h"
+
+using std::string;
+using std::vector;
+
+Studentas ivesk(bool randomMode);
+void parodykFailuSarasa();
+void issaugokIFaila(const vector<Studentas> &Grupe, const string &failoVardas);
+void issaugokPadalintusStudentus(const vector<Studentas> &Vargsai, const vector<Studentas> &Kietiakiai);
+
+vector<Studentas> nuskaitykIsFailo(const string &failoVardas);
+void generuotiStudentuFailus();
+
+double median(vector<int> v);
+double apskaiciuotiGalutiniVid(const Studentas &s);
+double apskaiciuotiGalutiniMed(const Studentas &s);
+
+void rusiuokStudentus(vector<Studentas> &Grupe);
+void padalinkStudentus(const vector<Studentas> &Grupe, vector<Studentas> &Vargsai, vector<Studentas> &Kietiakiai);
+bool palyginkStudentusPagalSkaitineReiksme(const Studentas &a, const Studentas &b);
+
+bool arSkaicius(const string &str);
+int gautiSkaiciu(const string &pranesimas);
+int gautiSkaitineReiksmeIsPavardes(const string& pavarde);
+
+#endif
---
+++ studentas.h
@@ -0,0 +1,19 @@
+#ifndef STUDENTAS_H_INCLUDED
+#define STUDENTAS_H_INCLUDED
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+struct Studentas {
+    string vard;
+    string pav;
+    int egzas;
+    double rez_vid;
+    double rez_med;
+    vector<int> paz;
+};
+
+#endif
