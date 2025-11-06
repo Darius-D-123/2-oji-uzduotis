cea7e96ef871676fa07ff795e10c22ac77a7957c
Darius-D-123 <KreeperLT123@gmail.com>
2025-11-06 22:11:35 +0200

    Update main.cpp v0.3 22:11

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include <string>
 #include <limits>
 #include <cstdlib>
@@ -12,74 +13,144 @@ using std::cin;
 using std::endl;
 using std::string;
 using std::vector;
+using std::list;
 
 int main() {
-     cout << "Ar norite atlikti greicio testavima? (t/n): ";
+    cout << "Ar norite atlikti greicio testavima? (t/n): ";
     char testavimoPasirinkimas;
     cin >> testavimoPasirinkimas;
     cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     if (testavimoPasirinkimas == 't' || testavimoPasirinkimas == 'T') {
-        testuotiProgramosSparta();
+        cout << "Pasirinkite konteineri:\n";
+        cout << "1 - std::vector\n";
+        cout << "2 - std::list\n";
+        cout << "Jusu pasirinkimas (1 arba 2): ";
+        char konteinerioPasirinkimas;
+        cin >> konteinerioPasirinkimas;
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        if (konteinerioPasirinkimas == '1') {
+            cout << "\nPasirinkite testavimo varianta:\n";
+            cout << "1 - Failu kurimo testavimas\n";
+            cout << "2 - Programos veikimo testavimas (nuskaitymas/rusiavimas/isvedimas)\n";
+            cout << "Jusu pasirinkimas (1 arba 2): ";
+            string pasirinkimas;
+            getline(cin, pasirinkimas);
+            if (pasirinkimas == "1") {
+                cout << "         FAILU KURIMO TESTAVIMAS \n";
+                testuotiFailuKurima();
+            } else if (pasirinkimas == "2") {
+                cout << "\nPasirinkite failo dydi:\n";
+                cout << "1 - 1000 irasu\n";
+                cout << "2 - 10000 irasu\n";
+                cout << "3 - 100000 irasu\n";
+                cout << "4 - 1000000 irasu\n";
+                cout << "5 - 10000000 irasu\n";
+                cout << "Jusu pasirinkimas (1-5): ";
+                string dydzioPasirinkimas;
+                getline(cin, dydzioPasirinkimas);
+                int dydis = 0;
+                if (dydzioPasirinkimas == "1") dydis = 1000;
+                else if (dydzioPasirinkimas == "2") dydis = 10000;
+                else if (dydzioPasirinkimas == "3") dydis = 100000;
+                else if (dydzioPasirinkimas == "4") dydis = 1000000;
+                else if (dydzioPasirinkimas == "5") dydis = 10000000;
+                else {
+                    cout << "Netinkamas pasirinkimas! Naudojamas 1000 irasu.\n";
+                    dydis = 1000;
+                }
+                testuotiPasirinktaFaila<vector<Studentas>>(dydis);
+            } else {
+                cout << "Netinkamas pasirinkimas!\n";
+            }
+        } else if (konteinerioPasirinkimas == '2') {
+            cout << "\nPasirinkite failo dydi:\n";
+            cout << "1 - 1000 irasu\n";
+            cout << "2 - 10000 irasu\n";
+            cout << "3 - 100000 irasu\n";
+            cout << "4 - 1000000 irasu\n";
+            cout << "5 - 10000000 irasu\n";
+            cout << "Jusu pasirinkimas (1-5): ";
+            string dydzioPasirinkimas;
+            getline(cin, dydzioPasirinkimas);
+            int dydis = 0;
+            if (dydzioPasirinkimas == "1") dydis = 1000;
+            else if (dydzioPasirinkimas == "2") dydis = 10000;
+            else if (dydzioPasirinkimas == "3") dydis = 100000;
+            else if (dydzioPasirinkimas == "4") dydis = 1000000;
+            else if (dydzioPasirinkimas == "5") dydis = 10000000;
+            else {
+                cout << "Netinkamas pasirinkimas! Naudojamas 1000 irasu.\n";
+                dydis = 1000;
+            }
+            testuotiPasirinktaFaila<list<Studentas>>(dydis);
+        } else {
+            cout << "Netinkamas pasirinkimas! Naudojamas std::vector.\n";
+            testuotiProgramosSparta();
+        }
+        cout << "        TESTAVIMAS BAIGTAS\n";
     } else {
-    srand(time(nullptr));
-    vector<Studentas> Grupe;
-    char pasirinkimas;
-    cout << "Ar norite generuoti studentu failus? (t/n): ";
-    char generuotiFailus;
-    cin >> generuotiFailus;
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    if (generuotiFailus == 't' || generuotiFailus == 'T') {
-        generuotiStudentuFailus();
-        cout << "Studentu failai sugeneruoti sekmingai!\n\n";
-    }
-    do {
-    cout << "Pasirinkite veiksma:\n";
-    cout << "  t - generuoti atsitiktinai\n";
-    cout << "  n - ivesti ranka\n";
-    cout << "  f - nuskaityti is failo\n";
-    cin >> pasirinkimas;
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-       } while (pasirinkimas != 't' && pasirinkimas != 'T' &&
-             pasirinkimas != 'n' && pasirinkimas != 'N' &&
-             pasirinkimas != 'f' && pasirinkimas != 'F');
-    if (pasirinkimas == 'f' || pasirinkimas == 'F') {
-        parodykFailuSarasa();
-        int failoPasirinkimas = gautiSkaiciu("Pasirinkite faila (iveskite skaiciu 1-6): ");
-        string failoVardas;
-        switch (failoPasirinkimas) {
-            case 1: failoVardas = "studentai1000.txt"; break;
-            case 2: failoVardas = "studentai10000.txt"; break;
-            case 3: failoVardas = "studentai100000.txt"; break;
-            case 4: failoVardas = "studentai1000000.txt"; break;
-            case 5: failoVardas = "studentai10000000.txt"; break;
-            case 6: failoVardas = "kursiokai.txt"; break;
-            default:
-                cout << "Netinkamas pasirinkimas! Naudojamas studentai10000.txt\n";
-                failoVardas = "studentai10000.txt";
-                break;
+        srand(time(nullptr));
+        vector<Studentas> Grupe;
+        char pasirinkimas;
+        cout << "Ar norite generuoti studentu failus? (t/n): ";
+        char generuotiFailus;
+        cin >> generuotiFailus;
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        if (generuotiFailus == 't' || generuotiFailus == 'T') {
+            generuotiStudentuFailus();
+            cout << "Studentu failai sugeneruoti sekmingai!\n\n";
         }
-        cout << "Skaitomas failas: " << failoVardas << endl;
-        Grupe = nuskaitykIsFailo(failoVardas);
-        if (Grupe.empty()) {
-            cout << "Nepavyko nuskaityti studentu duomenu. Programa baigia darba.\n";
-            return 1;
+        do {
+            cout << "Pasirinkite veiksma:\n";
+            cout << "  t - generuoti atsitiktinai\n";
+            cout << "  n - ivesti ranka\n";
+            cout << "  f - nuskaityti is failo\n";
+            cin >> pasirinkimas;
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } while (pasirinkimas != 't' && pasirinkimas != 'T' &&
+                 pasirinkimas != 'n' && pasirinkimas != 'N' &&
+                 pasirinkimas != 'f' && pasirinkimas != 'F');
+        if (pasirinkimas == 'f' || pasirinkimas == 'F') {
+            parodykFailuSarasa();
+            int failoPasirinkimas = gautiSkaiciu("Pasirinkite faila (iveskite skaiciu 1-6): ");
+            string failoVardas;
+            switch (failoPasirinkimas) {
+                case 1: failoVardas = "studentai1000.txt"; break;
+                case 2: failoVardas = "studentai10000.txt"; break;
+                case 3: failoVardas = "studentai100000.txt"; break;
+                case 4: failoVardas = "studentai1000000.txt"; break;
+                case 5: failoVardas = "studentai10000000.txt"; break;
+                case 6: failoVardas = "kursiokai.txt"; break;
+                default:
+                    cout << "Netinkamas pasirinkimas! Naudojamas studentai10000.txt\n";
+                    failoVardas = "studentai10000.txt";
+                    break;
+            }
+            cout << "Skaitomas failas: " << failoVardas << endl;
+            Grupe = nuskaitykIsFailo<vector<Studentas>>(failoVardas);
+
+            if (Grupe.empty()) {
+                cout << "Nepavyko nuskaityti studentu duomenu. Programa baigia darba.\n";
+                return 1;
+            }
+        } else {
+            int kiek = gautiSkaiciu("Kiek studentu norite ivesti? ");
+            bool randomMode = (pasirinkimas == 't' || pasirinkimas == 'T');
+            for (int j = 0; j < kiek; j++) {
+                cout << "\n--- Studentas " << j + 1 << " ---\n";
+                Grupe.push_back(ivesk(randomMode));
+            }
         }
-    } else {
-        int kiek = gautiSkaiciu("Kiek studentu norite ivesti? ");
-        bool randomMode = (pasirinkimas == 't' || pasirinkimas == 'T');
-        for (int j = 0; j < kiek; j++) {
-            cout << "\n--- Studentas " << j + 1 << " ---\n";
-            Grupe.push_back(ivesk(randomMode));
+        if (!Grupe.empty()) {
+            rusiuokStudentus(Grupe);
+            issaugokIFaila(Grupe, "rez.txt");
+            vector<Studentas> Vargsai, Kietiakiai;
+            padalinkStudentus(Grupe, Vargsai, Kietiakiai);
+            issaugokPadalintusStudentus(Vargsai, Kietiakiai);
         }
     }
-    if (!Grupe.empty()) {
-        rusiuokStudentus(Grupe);
-        issaugokIFaila(Grupe, "rez.txt");
-        vector<Studentas> Vargsai, Kietiakiai;
-        padalinkStudentus(Grupe, Vargsai, Kietiakiai);
-        issaugokPadalintusStudentus(Vargsai, Kietiakiai);
-    }
+
     return 0;
-    }
 }
