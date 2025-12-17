7e61554878df9d5b9a96ca704b686bfd44da6084
Darius-D-123 <KreeperLT123@gmail.com>
2025-12-17 23:33:40 +0200

    Update testai_studentas.cpp v2.0
    This file contains unit tests for the Studentas class, covering default and parameterized constructors, copy and move semantics, and validation of data integrity.

---
+++ testavimai/testai_studentas.cpp
@@ -0,0 +1,84 @@
+#include "studentas.h"
+#include "funkcijos.h"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <utility>
+
+TEST_CASE("Default constructor sukuria tuscia studenta", "[ctor][default]") {
+    Studentas s;
+
+    REQUIRE(s.vardas() == "");
+    REQUIRE(s.pavarde() == "");
+    REQUIRE(s.egzaminas() == 0);
+    REQUIRE(s.pazymiai().empty());
+    REQUIRE(s.rezultatasVidurkis() == Catch::Approx(0.0));
+    REQUIRE(s.rezultatasMediana() == Catch::Approx(0.0));
+    REQUIRE(s.isEmpty() == true);
+}
+
+TEST_CASE("Parametrized constructor nustato varda ir pavarde", "[ctor][param]") {
+    Studentas s("Vardenis", "Pavardenis");
+
+    REQUIRE(s.vardas() == "Vardenis");
+    REQUIRE(s.pavarde() == "Pavardenis");
+}
+
+TEST_CASE("Copy constructor nukopijuoja duomenis", "[copy][ctor]") {
+    Studentas a("Vardenis", "Pavardenis");
+    a.setPazymiai({5, 8, 7});
+    a.setEgzaminas(5);
+
+    Studentas b(a);
+    REQUIRE(b == a);
+}
+
+TEST_CASE("Copy assignment operator nukopijuoja duomenis", "[copy][assign]") {
+    Studentas a("Vardenis", "Pavardenis");
+    a.setPazymiai({5, 8, 7});
+    a.setEgzaminas(5);
+
+    Studentas b;
+    b = a;
+
+    REQUIRE(b == a);
+}
+
+TEST_CASE("Move constructor perkelia duomenis", "[move][ctor]") {
+    Studentas a("Vardenis", "Pavardenis");
+    a.setPazymiai({5, 8, 7});
+    a.setEgzaminas(5);
+
+    Studentas b(std::move(a));
+
+    REQUIRE(b.vardas() == "Vardenis");
+    REQUIRE(b.pavarde() == "Pavardenis");
+    REQUIRE(b.egzaminas() == 5);
+    REQUIRE(b.pazymiai() == std::vector<int>({5, 8, 7}));
+
+    REQUIRE(a.egzaminas() == 0);
+    REQUIRE(a.rezultatasVidurkis() == Catch::Approx(0.0));
+    REQUIRE(a.rezultatasMediana() == Catch::Approx(0.0));
+}
+
+TEST_CASE("Move assignment operator perkelia duomenis", "[move][assign]") {
+    Studentas a("Vardenis", "Pavardenis");
+    a.setPazymiai({5, 8, 7});
+    a.setEgzaminas(5);
+
+    Studentas b("X", "Y");
+    b = std::move(a);
+
+    REQUIRE(b.vardas() == "Vardenis");
+    REQUIRE(b.pavarde() == "Pavardenis");
+    REQUIRE(b.egzaminas() == 5);
+    REQUIRE(b.pazymiai() == std::vector<int>({5, 8, 7}));
+
+    REQUIRE(a.egzaminas() == 0);
+    REQUIRE(a.rezultatasVidurkis() == Catch::Approx(0.0));
+    REQUIRE(a.rezultatasMediana() == Catch::Approx(0.0));
+}
